ee7728a541672fff865e6198b82748b8b8f11239
Okko Ailio <oiaili@utu.fi>
2025-01-07 12:26:46 +0200

    Added fonts and some early door setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/_shared.scss"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <div>
+    <h1>WELCOME TO THE GAME PROGRAMME OF HBC STUDENT NIGHT 2025!</h1>
+    <p>Get ready for an electrifying night of basketball as [Team A] takes on [Team B] 
+      in a showdown of skill, determination, and team spirit! Whether you’re here to 
+      cheer for your favorite players, enjoy the energy of the game, or experience the 
+      thrill of live basketball, we’re excited to have you with us.</p>
+    <h2>WE WISH TO THANK</h2>
+  </div>
+)
 
 export default App
